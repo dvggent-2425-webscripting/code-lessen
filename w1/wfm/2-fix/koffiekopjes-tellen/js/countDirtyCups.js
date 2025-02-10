786706a2d1d99528dff89984fad2d6e8b2760b62
Sjouwke Van Parys <sjouwkevanparys@hotmail.com>
2025-02-10 21:34:04 +0100

    feat: add week 1

---
+++ w1/wfm/2-fix/koffiekopjes-tellen/js/countDirtyCups.js
@@ -0,0 +1,5 @@
+// Los de foutjes op zodat er een alert te zien is met het juiste aante vieze kopjes
+document.addEventListener("DOMContentLoaded", () => {
+  const dirtyCups = document.querySelectorAll("dirty-cups");
+  `Er zijn ${dirtyCups.length} vieze koffiekopjes!`;
+});
---
+++ w1/wfm/3-make/wie-is-het-molletje/js/main.js
@@ -0,0 +1,16 @@
+/**
+ * De volgende code zorgt ervoor dat de functie revealAnwser() wordt uitgevoerd
+ * als je op de button hebt geklikt.
+ * */
+const button = document.querySelector("button.reveal");
+button.addEventListener("click", () => revealAnswer());
+
+/* 1. TODO: Maak een variabele aan die de voornaam van de mol bijhoudt */
+// ...
+
+function revealAnswer() {
+  /* 2. TODO: Voeg de classe 'molletje' toe aan het juiste element uit de DOM */
+  //
+  /* 3. TODO: Toon een alert met daarin de volledige naam van het molletje */
+  //
+}
