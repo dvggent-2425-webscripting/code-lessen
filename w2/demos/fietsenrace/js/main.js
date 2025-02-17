829c15029342327f77b8ae30323ef0c9b10b54a8
Sjouwke Van Parys <sjouwkevanparys@hotmail.com>
2025-02-17 23:35:45 +0100

    feat(w2): add demo's without JS and add Watch excercises

---
+++ w2/demos/fietsenrace/js/main.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Haal de button uit de DOM en hang er een EventListener aan die de startRace() functie oproept.
+});
+
+function startRace() {
+  // 1. Haal de fietsen op uit de DOM
+  // ...
+  //
+  //
+  // 2. Bereken willekeurige snelheden per fiets. Zorg dat ze minstens 5 seconden (=5000 ms) erover doen.
+  // ...
+  //
+  //
+  // 3. Voeg deze snelheden toe als waarde van de transition-duration property van de fietsen. Voeg er "ms" aan toe.
+  // ...
+  //
+  //
+  // 4.Voeg de klasse 'active' toe aan de elementen, zodat ze beginnen 'wobblen'.
+  // ...
+  //
+  //
+  // 5. Verplaats de elementen 90% naar rechts.
+  // ...
+  //
+  //
+  // 6. Laat een alert verschijnen nadat de fiets die het traagst is, binnen is en verwijder de 'active' class.
+  // setTimeout(() => {
+  //   let winner;
+  //   if (bike1Speed < bike2Speed) {
+  //     winner = "Bike 1";
+  //   } else {
+  //     winner = "Bike 2";
+  //   }
+  //   bike1.classList.remove("active");
+  //   bike2.classList.remove("active");
+  //   alert(winner + " wins!");
+  // }, Math.max(bike1Speed, bike2Speed));
+}
---
+++ w2/demos/mysterious-buttons/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // 1. Variabelen declareren & elementen ophalen
+  // 2. Magische Kleur
+  // 3. Toon Geheim
+  // 4. Verberg Geheim
+  // 5. Gloeiend Effect
+});
