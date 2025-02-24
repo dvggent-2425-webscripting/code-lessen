1e9f38625e312d5ac53256ce6ab437771edde072
jacques <janneslambrecht@hotmail.com>
2025-02-24 22:38:40 +0100

    w3 - update: ticketberekening

---
+++ w3/klasoefening/ticketberekening/dynamic/js/main.js
@@ -0,0 +1,25 @@
+// Functie om data op te halen uit een HTML-element via een data-attribuut
+
+// Functie om tekstinhoud naar een HTML-element te schrijven
+
+// Functie om meerdere elementen te updaten met dezelfde data
+
+// Functie om één specifiek element te updaten met data
+
+// Ophalen van elementen die het ticketnummer tonen
+
+// Ophalen van datumgegevens en invullen van de juiste velden
+
+// Ophalen van shownaam en invullen in de titel- en ondertitelvelden
+
+// Ophalen van tijdsgegevens en invullen op de juiste plekken
+
+// Ophalen van locatiegegevens en invullen in de juiste velden
+
+// Updaten van het tijdschema aan de rechterkant
+
+// Ophalen en tonen van de barcode-afbeelding
+
+// Winkelwagentje functionaliteit
+
+// Eventlisteners toevoegen aan knoppen om tickets toe te voegen
