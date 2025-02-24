545f9b468ae1a9f2e0c88618521e905d63aa7a89
Sjouwke Van Parys <sjouwkevanparys@hotmail.com>
2025-02-24 21:05:49 +0100

    feat: add week 3

---
+++ w3/wfm/3-make/loading-screen/js/main.js
@@ -0,0 +1,9 @@
+// 1. Voeg een eventListener toe aan de button die het click event opvangt en daarna volgende functionaliteiten uitvoert:
+
+// 2. Haal de nodige elementen op
+
+// 3. Reset animatie
+
+// 4. Start animatie
+
+// 5. Toon boodschap na 3 seconden
---
+++ w3/wfm/3-make/slide-in/js/script.js
@@ -0,0 +1,5 @@
+// 1. Hou het aantal kliks bij in een variabele met data type Number
+
+// 2. Vang een klik event op op alle buttons
+
+// 3. Vang een klik event op op alle close buttons. Indien er geklikt wordt, verberg het parent element dan terug.
