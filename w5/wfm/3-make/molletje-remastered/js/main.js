f42d4967920b583d7a6092d6b2fd5fedd9cfe51e
Sjouwke Van Parys <sjouwkevanparys@hotmail.com>
2025-03-10 20:21:36 +0100

    feat(w5): add wfm

---
+++ w5/wfm/3-make/molletje-remastered/js/main.js
@@ -0,0 +1,17 @@
+// TODO 1: Maak een array van kandidaten aan.
+const candidates = [];
+
+// TODO 2: Sla de mol op in een variabele.
+const deMol = {};
+
+document.addEventListener("DOMContentLoaded", () => {
+  generateCandidates();
+
+  // TODO 4: Voeg een event listener toe aan de knop
+});
+
+// TODO 3: Genereer de kandidaten in de DOM
+function generateCandidates() {}
+
+// TODO 5: Zoek en markeer de mol
+function revealAnswer() {}
